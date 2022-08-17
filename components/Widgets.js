765d7b230de709c336bfa762bbb590b702902e8d
Ankit Jha <ankitjha1st@gmail.com>
2022-08-17 19:43:31 +0530

    Made News component of Widgets section using API

--- components/Widgets.js
+++ components/Widgets.js
@@ -1,6 +1,10 @@
 import { SearchIcon } from "@heroicons/react/outline";
+import { useState } from "react";
+import News from "./News";
 
-export default function Widgets() {
+export default function Widgets({ newsResults }) {
+  // the number of articles to show in news section
+  const [articleNum, setArticleNum] = useState(3);
   return (
     // 3 divs for the search bar
     <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
@@ -14,8 +18,20 @@ export default function Widgets() {
           />
         </div>
       </div>
+      {/* search bar done */}
+      <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]">
+        <h4 className="font-bold text-xl px-4">"What's happening"</h4>
+        {/* news Articles */}
+        {newsResults.slice(0, articleNum).map((article) => (
+          <News key={article.title} article={article} />
+        ))}
+        <button
+          onClick={() => setArticleNum(articleNum + 3)}
+          className="text-blue-300 pl-4 pb-3 hover:text-blue-400"
+        >
+          Show more
+        </button>
+      </div>
     </div>
-    // 
-    
   );
 }
