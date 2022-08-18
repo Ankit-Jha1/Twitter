3b10cf281ab0647263b38434d9cf10a50902d800
Ankit Jha <ankitjha1st@gmail.com>
2022-08-18 17:58:11 +0530

    fetching from Firebase and rendering on frontend

--- components/Feed.js
+++ components/Feed.js
@@ -1,43 +1,67 @@
 import { SparklesIcon } from "@heroicons/react/outline";
 import Input from "./Input";
 import Post from "./Post";
+import { useState, useEffect } from "react";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
 
 export default function Feed() {
-  const posts = [
-    {
-      id: "1",
-      name: "Gor",
-      username: "codewithgor",
-      UserImg:
-        "https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29vbCUyMGNhdHxlbnwwfHwwfHw%3D&w=1000&q=80",
-      // image posted by user
-      img: "https://images.unsplash.com/photo-1488415032361-b7e238421f1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
-      text: "Hoo hoo",
-      timestamp: "1 hour ago",
-    },
-    {
-      id: "2",
-      name: "illa",
-      username: "codewithilla",
-      UserImg:
-        "https://images.unsplash.com/photo-1590692995294-564cea87a687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
-      // image posted by user
-      img: "https://images.unsplash.com/photo-1501705388883-4ed8a543392c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      text: "Haa haa",
-      timestamp: "1 day ago",
-    },
-    {
-      id: "3",
-      name: "Tiger",
-      username: "codewithtiger",
-      UserImg:
-        "https://images.unsplash.com/photo-1501705388883-4ed8a543392c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      // image posted by user
-      img: "https://images.unsplash.com/photo-1564865878688-9a244444042a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      text: "Haa haa",
-      timestamp: "1 day ago",
-    },
-  ];
+  const [posts, setposts] = useState([]);
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "posts"), orderBy("timestamp", "desc")),
+      (snapshot) => {
+        setposts(snapshot.docs);
+      }
+    );
+  }, []);
+  const dummy = {
+    id: "1",
+    name: "Gor",
+    username: "codewithgor",
+    UserImg:
+      "https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29vbCUyMGNhdHxlbnwwfHwwfHw%3D&w=1000&q=80",
+    // image posted by user
+    img: "https://images.unsplash.com/photo-1488415032361-b7e238421f1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+    text: "Hoo hoo",
+    timestamp: "1 hour ago",
+  };
+
+  // const posts = [
+  //   {
+  //     id: "1",
+  //     name: "Gor",
+  //     username: "codewithgor",
+  //     UserImg:
+  //       "https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29vbCUyMGNhdHxlbnwwfHwwfHw%3D&w=1000&q=80",
+  //     // image posted by user
+  //     img: "https://images.unsplash.com/photo-1488415032361-b7e238421f1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+  //     text: "Hoo hoo",
+  //     timestamp: "1 hour ago",
+  //   },
+  //   {
+  //     id: "2",
+  //     name: "illa",
+  //     username: "codewithilla",
+  //     UserImg:
+  //       "https://images.unsplash.com/photo-1590692995294-564cea87a687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+  //     // image posted by user
+  //     img: "https://images.unsplash.com/photo-1501705388883-4ed8a543392c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  //     text: "Haa haa",
+  //     timestamp: "1 day ago",
+  //   },
+  //   {
+  //     id: "3",
+  //     name: "Tiger",
+  //     username: "codewithtiger",
+  //     UserImg:
+  //       "https://images.unsplash.com/photo-1501705388883-4ed8a543392c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  //     // image posted by user
+  //     img: "https://images.unsplash.com/photo-1564865878688-9a244444042a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  //     text: "Haa haa",
+  //     timestamp: "1 day ago",
+  //   },
+  // ];
   return (
     // Header -> Home on top with an icon
     <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
@@ -49,6 +73,16 @@ export default function Feed() {
       </div>
       {/* Input component for user to tweet */}
       <Input />
+
+      {/* firePosts */}
+      {/* {
+      fireposts.map( (firepost)=>(
+        <Post key={firepost.id} post={firepost} />
+      ))
+      } */}
+
+      {/* <Post key={dummy.id} post={dummy} /> */}
+
       {/* Posts */}
       {posts.map((post) => (
         <Post key={post.id} post={post} />
